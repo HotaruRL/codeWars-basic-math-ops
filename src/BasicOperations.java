9c38441f01ed26e796bd506c852b44db31aeb751
Hau Luc <richardluchau@protonmail.com>
2025-04-14 20:22:53 -0700

    version 1.0

--- src/BasicOperations.java
+++ src/BasicOperations.java
@@ -1,5 +1,20 @@
 public class BasicOperations {
-    public static void main(String[] args) {
 
+    public static int basicMath(String op, int v1, int v2) {
+        int result = 0;
+
+        if (op.equals("+")){
+            result = v1 + v2;
+        }else if (op.equals("-")){
+            result = v1 - v2;
+        }else if (op.equals("*")){
+            result = v1 * v2;
+        }else if (op.equals("/")) {
+            result = v1 / v2;
+        }else{
+            System.out.println("Please enter one of these 4 math ops: + - * /");
+        }
+
+        return result;
     }
 }
